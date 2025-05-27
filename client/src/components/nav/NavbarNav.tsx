4398c8cd7c20748cfdeaacaeb39720034742428e
Sadim Mali <sadimmalakar77@gmail.com>
2025-05-27 22:53:42 +0545

    feat: Enhance NavbarNav component with responsive design and additional links

--- client/src/components/nav/NavbarNav.tsx
+++ client/src/components/nav/NavbarNav.tsx
@@ -1,5 +1,57 @@
+import React from "react";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import { Heart, ShoppingCart, Users } from "lucide-react";
+import Link from "next/link";
+
+export const navLinks: {
+  id: number;
+  icon: React.ReactNode;
+  name: string;
+  route: string;
+}[] = [
+  {
+    id: 1,
+    icon: <Heart width={20} height={20} />,
+    name: "Favorite",
+    route: "/favorites",
+  },
+  {
+    id: 2,
+    icon: <ShoppingCart width={20} height={20} />,
+    name: "Cart",
+    route: "/cart",
+  },
+  {
+    id: 3,
+    icon: <Users width={20} height={20} />,
+    name: "Profile",
+    route: "/hero",
+  },
+];
+
 export default function NavbarNav() {
+
   return (
-    <div>NavbarNav</div>
-  )
+    <nav className="py-5">
+      <MaxWidthWrapper className="flex justify-between">
+        {/* brand logo */}
+        <div>
+          <Link href='/' className="font-semibold"> Tour</Link>
+        </div>
+        {/* nav links */}
+        <div className="flex gap-4">
+          {navLinks.map((nv) => (
+            <Link
+            href={nv.route}
+              key={nv.id}
+              className="understroke flex flex-col gap-1 items-center px-2  font-serif font-bold text-[#63687a]"
+            >
+              {nv.icon}
+              <span className="hidden md:block text-sm">{nv.name}</span> 
+            </Link>
+          ))}
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
 }
