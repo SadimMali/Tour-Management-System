aa84c0614f5410cd4673ed48ef1a58e3a8618ada
Sadim Mali <sadimmalakar77@gmail.com>
2025-05-28 15:05:00 +0545

    chore: add shadcn components

--- client/src/components/nav/NavbarNav.tsx
+++ client/src/components/nav/NavbarNav.tsx
@@ -21,35 +21,37 @@ export const navLinks: {
     name: "Cart",
     route: "/cart",
   },
-  {
-    id: 3,
-    icon: <Users width={20} height={20} />,
-    name: "Profile",
-    route: "/hero",
-  },
 ];
 
 export default function NavbarNav() {
-
   return (
-    <nav className="py-5">
+    <nav className=" sticky top-0 z-10 mb-6 py-5 w-full bg-white">
       <MaxWidthWrapper className="flex justify-between">
         {/* brand logo */}
         <div>
-          <Link href='/' className="font-semibold"> Tour</Link>
+          <Link href="/" className="font-semibold font-mono text-red-400">
+            Tour
+          </Link>
         </div>
         {/* nav links */}
         <div className="flex gap-4">
           {navLinks.map((nv) => (
             <Link
-            href={nv.route}
+              href={nv.route}
               key={nv.id}
-              className="understroke flex flex-col gap-1 items-center px-2  font-serif font-bold text-[#63687a]"
+              className="understroke flex flex-col gap-1 items-center px-2 font-bold text-[#63687a]"
             >
               {nv.icon}
-              <span className="hidden md:block text-sm">{nv.name}</span> 
+              <span className="hidden md:block text-sm">{nv.name}</span>
             </Link>
           ))}
+          {/* dropdown menu */}
+          <button className="understroke flex flex-col gap-1 items-center font-bold text-[#63687a]">
+              <Users width='20' height='20' />
+              <span className="hidden md:block text-sm">
+                  Profile
+              </span>
+          </button>
         </div>
       </MaxWidthWrapper>
     </nav>
